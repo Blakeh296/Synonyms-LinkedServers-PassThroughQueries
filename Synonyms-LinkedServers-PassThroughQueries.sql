9165c14437b158304264cd88ffaf8e2a82d10a38
Blake Hoelle <bhoelle216@gmail.com>
2018-11-07 21:06:47 -0500

    Added pass through queries

--- Synonyms-LinkedServers-PassThroughQueries.sql
+++ Synonyms-LinkedServers-PassThroughQueries.sql
@@ -26,8 +26,21 @@ GO
 CREATE SYNONYM [dbo].[AdvWorks2012ContactType] FOR [PL11\MTCDB].[AdventureWorks2012].[Person].[ContactType]
 GO
 
+-- PASS THROUGH QUERIES : Must have a linked database already in order for this to work
+-- YOU NEED THE V INFRONT OF THE TABLE NAME FOR IT TO WORK
 
+								/* Syntax OPENQUERY (linked_server, 'query') */
+SELECT * FROM OPENQUERY ([PL11\MTCDB], 'SELECT LastName, FirstName FROM AdventureWorks2012.HumanResources.vEmployee');
 
+SELECT * FROM OPENQUERY ([PL11\MTCDB],'SELECT AddressLine1, City, PostalCode FROM AdventureWorks2012.Person.Address');
 
+-- It is also possible to send UPDATE, INSERT and DELETE commands in a pass-through query.
 
+-- Run UPDATE statement the SELECT statement
+UPDATE OPENQUERY ([PL11\MTCDB], 'SELECT PhoneNumber FROM AdventureWorks2012.Person.PersonPhone WHERE BusinessEntityID = 1')
+SET PhoneNumber = '352-454-9993';
+SELECT * FROM OPENQUERY ([PL11\MTCDB], 'SELECT PhoneNumber FROM AdventureWorks2012.Person.PersonPhone WHERE BusinessEntityID = 1' );
 
+-- Run DELETE statement then SELECT statement
+DELETE OPENQUERY ([PL11\MTCDB] , 'SELECT PhoneNumber FROM AdventureWorks2012.Person.PersonPhone WHERE BusinessEntityID = 2');
+SELECT * FROM OPENQUERY ([PL11\MTCDB], 'SELECT BusinessEntityID, PhoneNumber FROM AdventureWorks2012.Person.PersonPhone');
